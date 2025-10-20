201c8c159890fddbdac65e9377688c4a5299ee4f
uvsq22102295 <emmy.marie-joseph@ens.uvsq.fr>
2025-10-20 11:58:01 +0200

    changement de la méthode boolean en public static

--- src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -7,21 +7,23 @@ public class AdresseIP {
 
     // Constructeur
     public AdresseIP(String ip) {
-        if(!isValid(ip)) {
+        if (!isValid(ip)) {
             throw new IllegalArgumentException("Adresse IP invalide : " + ip);
         }
         this.ip = ip;
     }
 
     // Méthode isValid()
-    private boolean isValid(String ip) {
+    public static boolean isValid(String ip) {
         // Verifier que c'est bien une adresse IPv4 !
         String[] parts = ip.split("\\.");
-        if(parts.length != 4) return false;
-        for(String part : parts) {
+        if (parts.length != 4)
+            return false;
+        for (String part : parts) {
             try {
                 int n = Integer.parseInt(part);
-                if(n < 0 || n > 255) return false;
+                if (n < 0 || n > 255)
+                    return false;
             } catch (NumberFormatException e) {
                 return false;
             }
@@ -43,8 +45,10 @@ public class AdresseIP {
     // Méthode equals()
     @Override
     public boolean equals(Object o) {
-        if(this == o) return true;
-        if(!(o instanceof AdresseIP)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof AdresseIP))
+            return false;
         AdresseIP other = (AdresseIP) o;
         return ip.equals(other.ip);
     }
@@ -55,4 +59,12 @@ public class AdresseIP {
         return ip.hashCode();
     }
 
+    // conrtit une adresse IP en une valeur numérique pour le tri (ls -a)
+    public long toNumeric() {
+        String[] parts = this.ip.split("\\.");
+        return (Long.parseLong(parts[0]) << 24)
+                | (Long.parseLong(parts[1]) << 16)
+                | (Long.parseLong(parts[2]) << 8)
+                | Long.parseLong(parts[3]);
+    }
 }
